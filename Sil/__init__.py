5e227151ad76dca7537285efb58515dc0c0f8402
Rand01ph <361212419@qq.com>
2015-01-15 15:31:13 +0800

    fix

---
+++ Sil/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# author: Rand01ph <tanyawei1991@gmail.com>
+
+from Sil.application import create_app
---
+++ Sil/helpers.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+
+# author: Rand01ph <Rand01ph@gmail.com>
